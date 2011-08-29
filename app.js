c6c94560a5f8a0f62e60b66c19a96e9fa46dc0dd
Stephane Bisson <stephane.c.bisson@gmail.com>
2011-08-29 16:14:54 -0600

    some change

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 
-mainController = function(){
+mainController = function($xhr){
     this.title = 'Write some code and try to make the tests green!';
     
     this.code = 'some code here\n\nand then some more';
     
-    this.tests = [
-        {content: 'do this'},
-        {content: 'do that'}
-    ];
+    this.tests = [{content: 'temp data'}];
+    var self = this;
+    // $.get('/couch/tests/_all_docs?include_docs=true', function(data) {
+    //     self.tests = jQuery.parseJSON(data).rows;
+    //     alert('Load was performed.');
+    // });
+    
+    $xhr('GET', '/couch/tests/_all_docs?include_docs=true', function(code, response) {
+        self.tests = response.rows;
+    });
     
     this.runTests = function(){
         this.tests.push({content: 'and yet another one'});
