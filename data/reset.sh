beb2d57d197e873015306cca90062a61b517d8d5
Stephane Bisson <stephane.c.bisson@gmail.com>
2011-08-30 13:42:14 -0600

    fixed the data setup script

--- data/reset.sh
+++ data/reset.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 url=http://127.0.0.1:5984
-db=tests2
+db=tests
+
+clear
 
 curl -X DELETE $url/$db
 curl -X PUT $url/$db
 
-curl -X POST --data @data1.json  -H "Content-Type:application/json" $url/$db
+for f in data*.json
+do
+    curl -X POST -d @$f  -H "Content-Type:application/json" $url/$db
+done
 
-curl $url/$db
+#curl $url/$db
