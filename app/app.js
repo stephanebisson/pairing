0e39c5871ba11b15758721e1fa7487c2bfe0497a
Stephane Bisson <stephane.c.bisson@gmail.com>
2012-09-01 19:35:55 +0800

    refresh that old idea a little bit

--- app/app.js
+++ app/app.js
@@ -1,20 +1,26 @@
 
-mainController = function($xhr){
-    this.title = 'Write some code and try to make the tests green!';
-    
-    this.code = 'var Math = {\n\tadd: function(a, b) {\n\t\treturn a + b;\n\t},\n\tsub: function(a, b) {\n\t\treturn a - b;\n\t}\n};';
-    
-    this.tests = [];
+var app = angular.module('app', []);
 
-    var self = this;
+app.controller('mainController', function($scope){
+    $scope.title = 'Write some code and try to make the tests green!';
+
+    $scope.tests = [
+        {seq: 1, content: 'expect(plus(1, 2)).toEqual(3);'},
+        {seq: 2, content: 'expect(plus(-2, 7)).toEqual(5);'}];
+        
+    $scope.level = 1;
     
-    $xhr('GET', '/couch/tests/_all_docs?include_docs=true', function(code, response) {
-        self.tests = response.rows;
-    });
-    
-    this.runTests = function(){
-        angular.forEach(this.tests, function(test){
-          test.success = !test.success;
-        });
+    var updateVisibleTests = function(){
+        $scope.testsInPlay = $scope.tests.slice(0, $scope.level);
     };
-};
+
+    $scope.run = function() {
+        angular.forEach($scope.testsInPlay, function(test){
+            test.fail = !(test.pass = (parseInt(Math.random()*10)%2 === 0));
+        });
+        $scope.level++;
+        updateVisibleTests();
+    };
+    
+    updateVisibleTests();
+});
