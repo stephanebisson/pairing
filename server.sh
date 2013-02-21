786a17eaf12f45ed8d976b0cd3fd2d7bdd7b267e
Stephane Bisson <stephane.c.bisson@gmail.com>
2013-02-20 19:10:40 -0500

    remove everything

--- server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar ./test/lib/jstestdriver/JsTestDriver.jar --port 9876 --browserTimeout 90000
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [ ! -e test.dissable ]; then
-  java -jar ./test/lib/jstestdriver/JsTestDriver.jar --tests all $@
-fi
